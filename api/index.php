35ae4331869f3a7eb89f0e40b18c8ec5223302e0
psz2007 <63392367+psz2007@users.noreply.github.com>
2023-04-10 13:32:47 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,15 +2,13 @@
 	try {
 		$curl = curl_init();
 		$url = "https://raw.githubusercontent.com/psz2007/random-pic/main/list.json";
-		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, $url);
 		$file = curl_exec($curl);
-		var_dump($file);
 		if($e = curl_error($curl)) {
 			die($e);
 		} else {
 			$pics = json_decode($file);
 		}
-		var_dump($pics);
 		$pic = 'https://raw.githubusercontent.com/psz2007/random-pic/main/' . $pics[array_rand($pics)];
 		die(header('Location: $pic'));
 	} catch (Exception $e) {
