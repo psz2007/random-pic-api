51b542db8d573f4c7ef8ccda0ed18a31cf1181a8
psz2007 <63392367+psz2007@users.noreply.github.com>
2023-04-10 21:22:11 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,10 @@
 	try {
 		$curl = curl_init();
 		$url = "https://raw.githubusercontent.com/psz2007/random-pic/main/list.json";
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, $url);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$file = curl_exec($curl);
 		if($e = curl_error($curl)) {
 			die($e);
